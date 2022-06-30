44da72d344b498b339f5dcbfa093552832ee9c67
Rohan Shahshankar <43114348+rohanpls@users.noreply.github.com>
2022-06-30 22:55:56 +0530

    Add more messages and randomize them

--- greeter.py
+++ greeter.py
@@ -1,6 +1,12 @@
+import random
+
 with open("ricebowl.txt", 'r') as ricebowl:
     print(ricebowl.read())
 
-msg = 'Welcome back! What do you wanna do today?\n'
+msg = ( "Welcome back! What do you wanna do today?",
+        "Ooooooo someone's in a hurry...",
+        "Battling some daemons today, are we?",
+        "How do you know if someone uses Arch? \n Don't worry, they'll tell you.",
+    )
 
-print(msg)
+print(random.choice(msg) + '\n')
