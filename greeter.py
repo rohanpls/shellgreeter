08eaa1d78b63fdde04e4173dee18c92fa1f435da
rohanpls <rnsh147@gmail.com>
2023-01-29 08:40:02 +0530

    Add humour prompts (won't print)

--- greeter.py
+++ greeter.py
@@ -28,5 +28,12 @@ msg = [ "Welcome back! What do you wanna do today?",
         "How do you know if someone uses Arch? \n Don't worry, they'll tell you.",
     ]
 
+humour = [
+        "Debugging: because sometimes your computer needs a good talking to.",
+        "I'm not arguing, I'm just explaining why I'm right.",
+        "A SQL query walks into a bar and sees two tables. He approaches, and asks 'Can I join you?'",
+        "Go on, make the computer do something funny.",
+        "press y to execute 'rm -rf /*'",
+        ]
 
 print(ricebowl, '\n' + choice(msg) + '\n')
