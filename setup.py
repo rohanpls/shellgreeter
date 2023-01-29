8d3ad3fcfddd8d47214461336409ae4ab0742bb5
rohanpls <rnsh147@gmail.com>
2023-01-29 09:01:28 +0530

    Add prompts to setup

--- setup.py
+++ setup.py
@@ -1,2 +1,16 @@
 # Script installer Init
 # Script by @rohanpls
+
+msg = [ "Welcome back! What do you wanna do today?",
+        "Someone's in a hurry...",
+        "Battling some daemons today, are we?",
+        "How do you know if someone uses Arch? \n Don't worry, they'll tell you."
+    ]
+
+humour = [
+        "Debugging: because sometimes your computer needs a good talking to.",
+        "I'm not arguing, I'm just explaining why I'm right.",
+        "A SQL query walks into a bar and sees two tables. He approaches, and asks 'Can I join you?'",
+        "Go on, make the computer do something funny.",
+        "press y to execute 'rm -rf /*'",
+        ]
