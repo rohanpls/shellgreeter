dc2cdc0205c4bc3e2ccec0dfb4cea49a80a0fb2b
rohanpls <rnsh147@gmail.com>
2023-01-29 12:03:57 +0530

    Declare legacy greeter v0.1

--- greeter-legacy.py
+++ greeter-legacy.py
@@ -1,3 +1,6 @@
+# Shell Greeter v0.1 - Ricing for your terminal!
+# Script by @rohanpls
+
 from random import choice
 
 ricebowl = """
@@ -27,13 +30,5 @@ msg = [ "Welcome back! What do you wanna do today?",
         "Battling some daemons today, are we?",
         "How do you know if someone uses Arch? \n Don't worry, they'll tell you.",
     ]
-
-humour = [
-        "Debugging: because sometimes your computer needs a good talking to.",
-        "I'm not arguing, I'm just explaining why I'm right.",
-        "A SQL query walks into a bar and sees two tables. He approaches, and asks 'Can I join you?'",
-        "Go on, make the computer do something funny.",
-        "press y to execute 'rm -rf /*'",
-        ]
 
 print(ricebowl, '\n' + choice(msg) + '\n')
