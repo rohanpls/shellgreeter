82ae302a59cb100f9232035456191a816c2721a5
rohanpls <rnsh147@gmail.com>
2023-01-29 09:40:14 +0530

    Add choice function

--- setup.py
+++ setup.py
@@ -1,16 +1,33 @@
 # Script installer Init
 # Script by @rohanpls
 
-msg = [ "Welcome back! What do you wanna do today?",
+gen = [ "Welcome back! What do you wanna do today?",
         "Someone's in a hurry...",
-        "Battling some daemons today, are we?",
-        "How do you know if someone uses Arch? \n Don't worry, they'll tell you."
+        "Battling some daemons today, are we?"
     ]
 
-humour = [
+hum = [
         "Debugging: because sometimes your computer needs a good talking to.",
         "I'm not arguing, I'm just explaining why I'm right.",
         "A SQL query walks into a bar and sees two tables. He approaches, and asks 'Can I join you?'",
         "Go on, make the computer do something funny.",
         "press y to execute 'rm -rf /*'",
         ]
+
+def choiceFunc():
+    c = 0
+    while c not in (1,2):
+        c = int(input("1. Generic prompts\n2. Humor prompts\n\nEnter choice: "))
+        if c not in (1,2):
+            print('\nInvalid choice. Please select an option from the list: ')
+    return c
+
+print("Select the prompts you want: ")
+choice = choiceFunc()
+f = open('ricebowl.py', 'a')
+if choice == 1:
+        chosen = gen
+        print(f"\nprompt = {chosen}\n")
+elif choice == 2:
+        chosen = hum
+        print(chosen)
